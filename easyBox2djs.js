9f34a9ac60648c6cede1a1793806666dc3bdaaf6
Jerome Etienne <jerome.etienne@gmail.com>
2011-02-09 07:29:43 +0100

    experimentation with dox to generate doc + early generic chained api 	modified:   Makefile 	new file:   easyBox2djs.html 	modified:   easyBox2djs.js

--- easyBox2djs.js
+++ easyBox2djs.js
@@ -1,4 +1,4 @@
-/**
+/*!
  * Chainable API on top of box2djs
  * - built using turing.js chain tutorial http://dailyjs.com/2010/08/26/framework-part-27/
  * initial stuff
@@ -10,13 +10,105 @@
  *   * bunch of attributes (key, val) for iClass[key]	= val;
  *   * attr({key: val})
  *   * bunch of attribute helpers, aka method in name of the key
+ *   * .get() return iClass
  *   * bunch of helpers which does more
 */
 
-//eb2Box().density(0.5).restitution(0).friction(1.0).toBody().position(80, 30).get()
-//eb2Box().attr({
 
+/**
+ * Define global namespace
+*/
 var eb2	= {};
+
+/**
+ * discover global namespace. support browser+nodejs
+*/
+eb2._global	= typeof window !== "undefined" ? window :
+			typeof global !== "undefined" ? global :
+			console.assert(false);
+
+/**
+ * Guess the attributes name based on the iClassName
+ * 
+ * - instantiate iClass and get all properties which are not function
+*/
+eb2._guessAttrNames	= function(iClassName){
+	var iClass	= new eb2._global[iClassName]()
+	var attrs	= [];
+	for(var key in iClass){
+		var type	= typeof iClass[key];
+		if( type === "function" )	continue;
+		attrs.push(key)
+	}
+	return attrs;
+}
+
+eb2._attrFunction	= function(param1, param2){
+	if(typeof param1 == "object"){
+		console.assert(typeof param2 == "undefined")
+		for(var key in param1){
+			this.attr(key, param1[key]);
+		}
+		return this;
+	}else if(typeof param2 == "undefined"){
+		// getter key/param1
+		return this._iClass[param1];
+	}else {
+		console.assert(typeof param2 != "undefined")
+		// setter key/param1 val/param2
+		this._iClass[param1]	= param2;
+	}
+	return this;
+}
+
+eb2._bindAttrHelpers	= function(){
+	// FIXIT is this self needed ???
+	var self	= this;
+	this._attrs.forEach(function(key){
+		self[key]	= function(val){
+			return self.attr(key, val)
+		};
+	})
+}
+
+eb2.getFunction	= function(){
+	return this._iClass;
+}
+
+/**
+ * Each object
+*/
+eb2._createClass	= function(opts){
+	// TODO to write
+	var className	= opts._className;
+
+	// ctor
+	eb2[className]	= function(ctorDef) {
+		return new eb2[className].fn.init(ctorDef);
+	}
+}
+
+// possible to guess _attrNames by inspecting a b2BoxDef
+eb2._createClass({
+	_className	: "boxDef",
+	_iClassName	: "b2BoxDef",
+	_attrNames	: eb2._guessAttrNames("b2BoxDef"),
+	toBodyDef	: function(){	return eb2.bodyDef(this._iClass)		},
+	size		: function(w, h){
+		this._iClass.extents.Set(w, h); return this;
+	}
+});
+
+eb2._createClass({
+	_className	: "BodyDef",
+	_iClassName	: "b2BodyDef",
+	_attrNames	: eb2._guessAttrNames("b2BodyDef"),
+	toBody		: function(world){	return world.CreateBody(this._iClass);	},
+	position	: function(x, y){
+		this._iClass.position.Set(x, y);
+		return this;
+	}
+})
 
 //////////////////////////////////////////////////////////////////////////////////
 //		eb2BoxDef							//
@@ -28,46 +120,21 @@ eb2.boxDef	= function(ctorDef) {
 
 eb2.boxDef.prototype = {
 	init		: function(ctorDef){
-		this._shapeDef	= ctorDef ? ctorDef : new b2BoxDef();
+		this._iClass	= ctorDef ? ctorDef : new b2BoxDef();
 		this._attrs	= ['density', 'restitution', 'friction'];
 		this._bindAttrHelpers();
 		return this;
 	},
-	get		: function(){
-		return this._shapeDef;
-	},
 	toBodyDef	: function(){
-		return eb2.bodyDef(this._shapeDef)
+		return eb2.bodyDef(this._iClass)
 	},
 	size		: function(w, h){
-		this._shapeDef.extents.Set(w, h);
+		this._iClass.extents.Set(w, h);
 		return this;
 	},
-	attr		: function(param1, param2){
-		if(typeof param1 == "object"){
-			console.assert(typeof param2 == "undefined")
-			for(var key in param1){
-				this.attr(key, param1[key]);
-			}
-			return this;
-		}else if(typeof param2 == "undefined"){
-			// getter key/param1
-			return this._shapeDef[param1];
-		}else {
-			console.assert(typeof param2 != "undefined")
-			// setter key/param1 val/param2
-			this._shapeDef[param1]	= param2;
-		}
-		return this;
-	},
-	_bindAttrHelpers: function(){
-		var self	= this;
-		this._attrs.forEach(function(key){
-			self[key]	= function(val){
-				return self.attr(key, val)
-			};
-		})
-	}
+	get		: eb2._getFunction,
+	attr		: eb2._attrFunction,
+	_bindAttrHelpers: eb2._bindAttrHelpers,
 };
 
 // magic line i dont understand
